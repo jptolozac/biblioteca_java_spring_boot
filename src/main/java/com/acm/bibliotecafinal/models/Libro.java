3fa05a4bfd22a2ebd2d2ae652a07d1a96de0a275
Hernando Ninco <hernando.ninco.at@usa.edu.co>
2024-05-13 20:32:03 -0500

    agregar libro

--- src/main/java/com/acm/bibliotecafinal/models/Libro.java
+++ src/main/java/com/acm/bibliotecafinal/models/Libro.java
@@ -14,13 +14,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
+@Builder
 @Entity
 public class Libro {
     @Id
